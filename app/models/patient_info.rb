e5fd229e1a843278c76b5d4e0575e3e11dba3004
allenwhitedev <allenwhitedev@gmail.com>
2015-01-11 13:13:15 -0500

    Remove spouse validations

--- app/models/patient_info.rb
+++ app/models/patient_info.rb
@@ -2,7 +2,7 @@ class PatientInfo < ActiveRecord::Base
 	belongs_to :patient
 	validates :patient_id, uniqueness: true
 
-	attributes = ["address", "email", "city", "state", "zipcode", "sex", "birthdate", "relationship_status", "employer_or_school", "occupation", "employer_or_school_address", "employer_or_school_phone", "spouse_name", "spouse_birthdate", "spouse_employer", "referrer"] 
+	attributes = ["address", "email", "city", "state", "zipcode", "sex", "birthdate", "relationship_status", "employer_or_school", "occupation", "employer_or_school_address", "employer_or_school_phone", "referrer"] 
 
 	attributes.each do |attribute|
 		validates_presence_of attribute
